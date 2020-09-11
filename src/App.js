464c3d4b0c339363ff40d3a2401280d0d14366d1
truong.nguyen <truong.nguyen@persol.co.jp>
2020-09-11 10:03:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import createPlotlyComponent from "react-plotlyjs";
+
+// import createPlotlyComponent from "react-plotly.js/factory";
+// import Plotly from "./custom-plotly";
+import Plotly from "plotly.js/dist/plotly-cartesian";
+
+const Plot = createPlotlyComponent(Plotly);
 
 function App() {
+  var trace1 = {
+    x: ["giraffes", "orangutans", "monkeys"],
+    y: [20, 14, 23],
+    name: "SF Zoo",
+    type: "bar",
+  };
+
+  var trace2 = {
+    x: ["giraffes", "orangutans", "monkeys"],
+    y: [12, 18, 29],
+    name: "LA Zoo",
+    type: "bar",
+  };
+  const data = [trace1, trace2];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Plotly Graphic React</h2>
+      <Plot
+        data={data}
+        layout={{
+          title: "A Fancy Plot",
+          barmode: "group",
+        }}
+      />
     </div>
   );
 }
---
+++ src/custom-plotly.js
@@ -0,0 +1,5 @@
+import Plotly, { register } from "plotly.js/lib/core";
+
+register([require("plotly.js/lib/pie"), require("plotly.js/lib/choropleth")]);
+
+export default Plotly;
